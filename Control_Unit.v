58b39602719d0b9a3d443a8485c05cabaee5ead6
Makari Green <129010277+xk4r1x@users.noreply.github.com>
2025-05-25 18:00:12 -0700

    Update Control_Unit.v

--- Control_Unit.v
+++ Control_Unit.v
@@ -1,26 +1,30 @@
 module Control_Unit (
-   input logic[6:0] opcode,
-   output logic RegWrite, MemtoReg, MemRead, MemWrite, ALUSrc, Branch,
-   output logic [1:0] ALUop 
+    input logic [6:0] opcode,
+    output logic RegWrite, MemtoReg, MemRead, MemWrite, ALUSrc, Branch,
+    output logic [1:0] ALUop
 );
-   
-   always_comb begin
-      case(opcode)
-         7'b0110011: begin //R-type
-            RegWrite = 1; ALUop = 2b'10; ALUSrc = 0; Branch = 0; MemWrite = 0; MemRead = 0; MemtoReg = 0;
-         end
-
-         7'b0000011: begin //Load
-            RegWrite = 1; ALUop = 2b'00; ALUSrc = 1; Branch = 0; MemWrite = 0; MemRead = 1; MemtoReg = 1;
-         end
-
-         7'0100011: begin //Store
-            RegWrite = 0; ALUop = 2b'00; ALUSrc = 1; Branch = 0; MemWrite = 1; MemRead = 0; MemtoReg = 0;
-         end
-
-         7'b1100011: begin //Branch
-            RegWrite = 0; ALUop = 2b'01; ALUSrc = 0; Branch = 0; MemWrite = 0; MemRead = 0; MemtoReg = 0;
-         end
-      endcase
-   end
+    always_comb begin
+        case(opcode)
+            7'b0110011: begin // R-type
+                RegWrite = 1; ALUop = 2'b10; ALUSrc = 0;
+                Branch = 0; MemWrite = 0; MemRead = 0; MemtoReg = 0;
+            end
+            7'b0000011: begin // Load
+                RegWrite = 1; ALUop = 2'b00; ALUSrc = 1;
+                Branch = 0; MemWrite = 0; MemRead = 1; MemtoReg = 1;
+            end
+            7'b0100011: begin // Store
+                RegWrite = 0; ALUop = 2'b00; ALUSrc = 1;
+                Branch = 0; MemWrite = 1; MemRead = 0; MemtoReg = 0;
+            end
+            7'b1100011: begin // Branch
+                RegWrite = 0; ALUop = 2'b01; ALUSrc = 0;
+                Branch = 1; MemWrite = 0; MemRead = 0; MemtoReg = 0;
+            end
+            default: begin
+                RegWrite = 0; ALUop = 2'b00; ALUSrc = 0;
+                Branch = 0; MemWrite = 0; MemRead = 0; MemtoReg = 0;
+            end
+        endcase
+    end
 endmodule
