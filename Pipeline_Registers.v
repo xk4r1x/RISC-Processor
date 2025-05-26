b5e379055cf06a5a4533ceccbd7efed360acf9b5
Makari Green <129010277+xk4r1x@users.noreply.github.com>
2025-05-25 18:03:32 -0700

    Update Pipeline_Registers.v

--- Pipeline_Registers.v
+++ Pipeline_Registers.v
@@ -1,12 +1,12 @@
 module Pipeline_Registers (
-   input logic clk, reset,
-   input logic [31:0] inData,
-   output logic [31:0] outData
+    input logic clk, reset,
+    input logic [31:0] inData,
+    output logic [31:0] outData
 );
-   always_ff@(posedge clk or posedge reset) begin
-      if (reset)
-         outData <= 0;
-      else
-         outData <= inData
-   end
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset)
+            outData <= 32'b0;
+        else
+            outData <= inData;
+    end
 endmodule
