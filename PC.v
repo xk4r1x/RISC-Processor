77cd9766cb15b2a0664a08598e91f878cbbbeff1
Makari Green <129010277+xk4r1x@users.noreply.github.com>
2025-05-25 18:01:13 -0700

    Update PC.v

--- PC.v
+++ PC.v
@@ -1,12 +1,12 @@
 module PC (
-   input wire clk, reset,
-   input wire[31:0] next_pc,
-   output reg[31:0] pc
+    input logic clk, reset,
+    input logic [31:0] next_pc,
+    output logic [31:0] pc
 );
-   always @(posedge clk or posedge reset) begin
-      if (reset)
-         pc <= 0;
-      else
-         pc <= next_pc
-   end
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset)
+            pc <= 32'b0;
+        else
+            pc <= next_pc;
+    end
 endmodule
