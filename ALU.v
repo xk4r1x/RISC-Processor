1dcb8ad33cc198ddcf3d95250e0aae3171dbb234
Makari Green <129010277+xk4r1x@users.noreply.github.com>
2025-05-25 17:58:02 -0700

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,19 +1,21 @@
 module ALU (
-   input logic[31:0] A, B,
-   input logic[3:0] ALUControl,
-   output logic[31:0] ALUResult,
-   output logic Zero
+    input logic [31:0] A, B,
+    input logic [3:0] ALUControl,
+    output logic [31:0] ALUResult,
+    output logic Zero
 );
+    always_comb begin
+        case(ALUControl)
+            4'b0000: ALUResult = A & B;
+            4'b0001: ALUResult = A | B;
+            4'b0010: ALUResult = A + B;
+            4'b0110: ALUResult = A - B;
+            4'b0111: ALUResult = (A < B) ? 32'd1 : 32'd0;
+            4'b1100: ALUResult = ~(A | B); // NOR
+            default: ALUResult = 32'd0;
+        endcase
+    end
 
-   always_comb begin
-      case(ALUControl)
-         4'b0000: ALUResult = A & B; // AND
-         4'b0001: ALUResult = A | B; // OR
-         4'b0010: ALUResult = A + B; // ADD
-         4'b0110: ALUResult = A - B; // SUB
-         4'b0111: ALUResult = (A < B) ? 1: 0; // SLT
-         4'b0000: ALUResult = ~(A | B); // NOR
-      endcase
-   end
-   assign Zero = (ALUResult == 0);   
+    assign Zero = (ALUResult == 0);
+
 endmodule
